6b7521c5a6a91a6cdba48e419dc6870a4e61b9f4
Shyla Earl <shyla.earl@gmail.com>
2021-04-22 14:31:40 -0500

    added GET end points to server and client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -29,8 +29,32 @@ function setupClickListeners() {
 
 function getKoalas(){
   console.log( 'in getKoalas' );
+  $("#viewKoalas").empty();
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koala'
+  }).then(function(response) {
+    console.log(response);
+    for(let i = 0; i < response.length; i += 1) {
+      let koala = response[i];
+      // For each book, append a new row to our table
+      $('#viewKoalas').append(`
+        <tr>
+          <td>${koala.name}</td>
+          <td>${koala.gender}</td>
+          <td>${koala.age}</td>
+          <td>${koala.readyForTransfer}</td>
+          <td>${koala.notes}</td>
+          <td><button class="transfer-koala" data-id="${koala.id}">Transfer</button></td>
+          <td><button class="remove-koala" data-id="${book.id}">Remove Koala</button></td>
+        </tr>
+      `);
+    }//end for loop
+    //renderKoalas(response);
+  }).catch(function(error){
+    console.log('error in GET', error);
+  });
 } // end getKoalas
 
 function saveKoala( newKoala ){
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,6 +5,17 @@ const koalaRouter = express.Router();
 
 
 // GET (Shyla)
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koala";';
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error getting koalas', error);
+      res.sendStatus(500);
+    });
+  });
 
 
 // POST (Reese)
