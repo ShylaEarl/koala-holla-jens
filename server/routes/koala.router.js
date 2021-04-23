00514f07295e377e91dac8b68c4b0506949960f4
Johnny Keple <Johnny.Keple@gmail.com>
2021-04-23 09:21:03 -0500

    Delete function

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,7 @@ const koalaRouter = express.Router();
 const pool = require('../modules/pool');
 
 // GET (Shyla)
-router.get('/', (req, res) => {
+koalaRouter.get('/', (req, res) => {
     let queryText = 'SELECT * FROM "koala";';
     pool.query(queryText).then(result => {
       // Sends back the results in an object
@@ -23,10 +23,11 @@ koalaRouter.post('/', (req,res) => {
   let newKoala = req.body;
   console.log('Adding new Koala...', newKoala);
 
-  let queryText = `INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes")
+  let queryText = `INSERT INTO "koala" ("name", "age", "gender", "ready_to_transer", "notes")
                   VALUES ($1, $2, $3, $4, $5);`;
 
-  pool.query(queryText, [newKoala.name, newKoala.gender, newKoala.age, newKoala.ready_to_transfer, newKoala.notes]) 
+
+  pool.query(queryText, [newKoala.name, newKoala.age, newKoala.gender, newKoala.ready_to_transer, newKoala.notes]) 
   .then(result => {
       res.sendStatus(201);
   })  
@@ -40,5 +41,20 @@ koalaRouter.post('/', (req,res) => {
 
 
 // DELETE
+koalaRouter.delete('/:id', (req, res) => {
+  let reqID = req.params.id;
+  console.log('Delete request id', reqID);
+  
+  let sqlText = 'DELETE FROM "koala" WHERE "id"=$1;';
+  pool.query(sqlText, [reqID])
+    .then((result) => {
+      console.log('Koala Deleted');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500);
+    })
+})
 
 module.exports = koalaRouter;
